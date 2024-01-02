6e2858c5e31191691d4b113d914fdd275f2ddb7f
AliZain82 <119394666+AliZain82@users.noreply.github.com>
2024-01-02 20:44:32 +0500

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database_with_LINQ.Models
+{
+    public class Class
+    {
+        [Key]public int cId { get; set; }
+        public string Name { get; set; }
+        public string RoomNumber { get; set; }
+        public int fId { get; set; }
+
+        public List<Enrollment> Enrolled { get; set; }
+        
+    }
+}
